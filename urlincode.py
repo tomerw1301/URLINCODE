9e59bd94f8643c6fecefc680401965720ccbf805
tomerw1301 <136242061+tomerw1301@users.noreply.github.com>
2023-07-23 14:41:50 +0300

    Add files via upload

---
+++ urlincode.py
@@ -0,0 +1,9 @@
+print('https://www.wikipedia.com')
+print('https://www.apple.com')
+print('https://www.samsung.com')
+print('https://www.facebook.com')
+print('https://www.cnn.com')
+print('https://www.twitter.com')
+print('https://www.instagram.com')
+print('https://www.9gag.com')
+
